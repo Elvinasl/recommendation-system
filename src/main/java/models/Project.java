aac1f31e53df0cae1b9f0027b53dc38f1c8f527d
Elvinas Lukasevicius <ell@open.ch>
2019-12-09 10:47:53 +0100

    User created, user-project relationship

--- src/main/java/models/Project.java
+++ src/main/java/models/Project.java
@@ -8,6 +8,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 @Data
@@ -32,4 +33,7 @@ public class Project {
     @Column(nullable = false)
     @NotNull
     private Client client;
+
+    @OneToMany(mappedBy = "project")
+    private List<User> users;
 }
---
+++ src/main/java/models/User.java
@@ -0,0 +1,24 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @Column(nullable = false)
+    @NotNull
+    private Project project;
+}
