7859a4a2ae930500415b3275d31ca7da7884c7d7
Elvinas Lukasevicius <ell@open.ch>
2019-12-09 12:02:42 +0100

    User preference initialised

--- src/main/java/models/UserPreference.java
+++ src/main/java/models/UserPreference.java
@@ -1,4 +1,30 @@
 package models;
 
-public class UserPreference {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import models.shared.Weight;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPreference extends Weight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @Column(nullable = false)
+    @NotNull
+    private User user;
+
+    @ManyToOne(optional = false)
+    @Column(nullable = false)
+    @NotNull
+    private Cell cell;
 }
