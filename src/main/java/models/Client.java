c1ab940b95ad6fdf1359d6b48d9f62a751581abc
Elvinas Lukasevicius <ell@open.ch>
2019-12-09 10:32:17 +0100

    Client modal added, gitignore updated, hibernate dependencies added

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -1,4 +1,25 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(nullable = false)
+    @NotEmpty
+    private String email;
+
+    @JsonIgnore
+    @NotEmpty
+    @Column(nullable = false)
+    private String password;
 }
