b26c2ff53690978226edb38d7476e0168acf9dec
Elvinas Lukasevicius <ell@open.ch>
2019-12-09 10:35:17 +0100

    Lambok dependency added

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -1,12 +1,18 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
 
     @Id
