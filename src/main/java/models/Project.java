abaf33ffe4180868b2866e6b5851a8a0eee605dd
Elvinas Lukasevicius <ell@open.ch>
2019-12-09 10:39:11 +0100

    Project model initialised

--- src/main/java/models/Project.java
+++ src/main/java/models/Project.java
@@ -1,9 +1,30 @@
 package models;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    @Length(min = 2, max = 45)
+    private String name;
+
+    @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String apiKey;
 
 }
