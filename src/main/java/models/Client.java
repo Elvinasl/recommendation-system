5c81ddd1bff683bae0ff0c8c2459450338d6864b
Elvinas Lukasevicius <ell@open.ch>
2019-12-15 12:22:35 +0100

    Role, permission and active added to client, security dependencies

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -32,4 +35,23 @@ public class Client {
 
     @OneToMany(mappedBy="client")
     private List<Project> project;
+
+    @NotNull
+    @JsonIgnore
+    private String role;
+
+    @NotNull
+    @JsonIgnore
+    private String permissions = "";
+
+    @NotNull
+    @JsonIgnore
+    private boolean activated = true;
+
+    public List<String> getPermissionList(){
+        if(this.permissions.length() > 0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
 }
