78d74eadcfc31b397f3364c0fb285ff11ecce291
Elvinas Lukasevicius <ell@open.ch>
2019-12-16 13:18:30 +0100

    Test controller cleanup

--- src/main/java/controllers/TestsController.java
+++ src/main/java/controllers/TestsController.java
@@ -1,24 +1,16 @@
 package controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import services.TestService;
 
 @RestController
 @RequestMapping("/test")
 public class TestsController {
 
-    private TestService testService;
-
-    @Autowired
-    public TestsController(TestService testService) {
-        this.testService = testService;
-    }
-
     @GetMapping
-    public String test() {
-        return testService.test();
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok( "Authenticated! Yaaahooo!");
     }
 }
