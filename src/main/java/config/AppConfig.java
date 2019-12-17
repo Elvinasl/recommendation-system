6c3ff4df1f64552b23b15e9c43c46686ee79ce16
Elvinas Lukasevicius <ell@open.ch>
2019-12-17 11:01:21 +0100

    App config code cleanup

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -3,16 +3,15 @@ package config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@Import(DatabaseConfig.class)
 @ComponentScans({
         @ComponentScan("controllers"),
         @ComponentScan("repositories"),
-        @ComponentScan("services")
+        @ComponentScan("services"),
+        @ComponentScan("config")
 })
 public class AppConfig {
 
