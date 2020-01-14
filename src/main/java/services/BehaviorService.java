29a0f5a1054e2ffe5a24e69e5e033df3fc6f7c3d
Elvinas Lukasevicius <ell@open.ch>
2020-01-14 14:30:29 +0100

    Behavior repository connected

--- src/main/java/services/BehaviorService.java
+++ src/main/java/services/BehaviorService.java
@@ -2,21 +2,25 @@ package services;
 
 import dto.BehaviorDTO;
 import exceptions.responses.Response;
+import models.Behavior;
 import models.Project;
 import models.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repositories.BehaviorRepository;
 
 @Service
 public class BehaviorService {
 
     private ProjectService projectService;
     private RowService rowService;
+    private BehaviorRepository behaviorRepository;
 
     @Autowired
-    public BehaviorService(ProjectService projectService, RowService rowService) {
+    public BehaviorService(ProjectService projectService, RowService rowService, BehaviorRepository behaviorRepository) {
         this.projectService = projectService;
         this.rowService = rowService;
+        this.behaviorRepository = behaviorRepository;
     }
 
     public Response add(String apiKey, BehaviorDTO behaviorDTO) {
@@ -24,6 +28,12 @@ public class BehaviorService {
 
         Row row = rowService.getRowByCellDTOAndProject(behaviorDTO.getCells(), project);
 
+        Behavior behavior = new Behavior();
+        behavior.setRow(row);
+        behavior.setLiked(behaviorDTO.isLiked());
+        // TODO: create used logic here
+//        behavior.setUser(null);
+        behaviorRepository.add(behavior);
         return new Response("Behavior recorded");
     }
 }
