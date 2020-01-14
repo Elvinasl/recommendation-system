523d98a826ebfc250434d2656e4b08b4fe1edaed
Elvinas Lukasevicius <ell@open.ch>
2020-01-14 10:04:04 +0100

    Row as a reserved name in mysql 8 workaround

--- src/main/java/models/Row.java
+++ src/main/java/models/Row.java
@@ -1,6 +1,5 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
+@Entity(name = "Row_")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
