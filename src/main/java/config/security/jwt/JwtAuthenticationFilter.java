6c5aff408434d97a6cc1d65b86ff0131966cbb83
Elvinas Lukasevicius <ell@open.ch>
2020-01-19 16:59:56 +0100

    Security cleanup

--- src/main/java/config/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/config/security/jwt/JwtAuthenticationFilter.java
@@ -2,11 +2,8 @@ package config.security.jwt;
 
 import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import config.security.AuthenticatedClientPOJO;
 import config.security.ClientPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,7 +11,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -80,9 +76,5 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         // Add token in response
         response.addHeader(JwtProperties.HEADER_STRING, fullToken);
         response.setContentType("application/json");
-        AuthenticatedClientPOJO authenticatedClientPOJO = new AuthenticatedClientPOJO();
-        authenticatedClientPOJO.setEmail(principal.getClient().getEmail());
-        authenticatedClientPOJO.setId(principal.getClient().getId());
-        response.getWriter().print(authenticatedClientPOJO.toString());
     }
 }
