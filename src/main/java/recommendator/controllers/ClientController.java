961c3e2909ec8de4e1038a5c1d76adcfb3c4371e
Elvinas Lukasevicius <ell@open.ch>
2020-01-21 13:40:44 +0100

    Spring security fix

--- src/main/java/recommendator/controllers/ClientController.java
+++ src/main/java/recommendator/controllers/ClientController.java
@@ -1,12 +1,9 @@
 package recommendator.controllers;
 
+import org.springframework.web.bind.annotation.*;
 import recommendator.models.entities.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import recommendator.services.ClientService;
 
 import javax.validation.Valid;
