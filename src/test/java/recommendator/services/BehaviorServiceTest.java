46fc1217d3115cfc7d08df40a850f6763433cf52
Elvinas Lukasevicius <ell@open.ch>
2020-01-23 10:03:11 +0100

    Tests folder structure

---
+++ src/test/java/recommendator/services/BehaviorServiceTest.java
@@ -0,0 +1,20 @@
+package recommendator.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BehaviorServiceTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getBehaviorsByUserAndTypeAndProject() {
+    }
+
+    @Test
+    void getBehaviorsByUser() {
+    }
+}
