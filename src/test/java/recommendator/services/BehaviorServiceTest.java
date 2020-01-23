fb32d89c9f74c8b05a4b2e2e1efc214a6d9c10c4
Thomas Christerus <tchristerus@outlook.com>
2020-01-23 11:15:49 +0100

    Add: Mockito test template

--- src/test/java/recommendator/services/BehaviorServiceTest.java
+++ src/test/java/recommendator/services/BehaviorServiceTest.java
@@ -1,55 +1,59 @@
 package recommendator.services;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import recommendator.dto.BehaviorDTO;
 import recommendator.exceptions.responses.Response;
 import recommendator.models.entities.Behavior;
+import recommendator.models.entities.Project;
+import recommendator.models.entities.Row;
 import recommendator.models.entities.User;
 import recommendator.repositories.BehaviorRepository;
+
 import static org.assertj.core.api.Assertions.assertThat;
-
-
-
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @ExtendWith(MockitoExtension.class)
 class BehaviorServiceTest {
 
-    @InjectMocks
-    private BehaviorService behaviorService;
+    @Mock ProjectService projectService;
+    @Mock RowService rowService;
+    @Mock UserService userService;
+    @Mock UserPreferenceService userPreferenceService;
+    @Mock BehaviorRepository behaviorRepository;
+    @Mock BehaviorService behaviorService;
 
-    @Mock
-    private BehaviorRepository behaviorRepository;
+    @BeforeEach
+    public void setUp() {
+        behaviorService = new BehaviorService(projectService, rowService, behaviorRepository, userService, userPreferenceService);
+    }
 
     @Test
     void add() {
 
-        List<Behavior> behaviors = Arrays.asList(new Behavior(), new Behavior());
-        Mockito.when(behaviorRepository.getBehaviorsByUser(any(User.class))).thenReturn(behaviors);
+        Mockito.when(projectService.getByApiKey(anyString())).thenReturn(new Project());
+        Mockito.when(rowService.getRowByCellDTOAndProject(anyList(), any(Project.class))).thenReturn(new Row());
+        Mockito.when(userService.findByExternalIdAndProjectOrNull(anyString(), any(Project.class))).thenReturn(new User());
+
+        Response response = behaviorService.add("test", new BehaviorDTO());
+        assertThat("Behavior recorded").isEqualTo(response.getMessage());
+        verify(behaviorRepository, times(1)).add(any(Behavior.class));
 
 
-        BehaviorDTO behaviorDTO = new BehaviorDTO(null, true, null);
+//        List<Behavior> behaviors = Arrays.asList(new Behavior(), new Behavior());
+//        Mockito.when(behaviorRepository.getBehaviorsByUser(any(User.class))).thenReturn(behaviors);
+//
+//
+//        BehaviorDTO behaviorDTO = new BehaviorDTO(null, true, null);
 
 
 //        assertThat(behaviorService.add("", behaviorDTO), new Response(""));
-        verify(behaviorRepository, times(1)).add(any(Behavior.class));
 
     }
 
