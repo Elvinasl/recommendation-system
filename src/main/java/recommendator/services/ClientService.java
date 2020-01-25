2111abc72ae395841ab401496559baadad1fe66b
Elvinas Lukasevicius <ell@open.ch>
2020-01-25 17:37:55 +0200

    Client administration controllers, service, repository

--- src/main/java/recommendator/services/ClientService.java
+++ src/main/java/recommendator/services/ClientService.java
@@ -1,8 +1,9 @@
 package recommendator.services;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import recommendator.dto.AllClientsDTO;
+import recommendator.dto.ClientDTO;
 import recommendator.dto.LoginDTO;
-import recommendator.exceptions.RowAlreadyExistsException;
 import recommendator.exceptions.SomethingWentWrongException;
 import recommendator.exceptions.responses.Response;
 import recommendator.models.entities.Client;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import recommendator.repositories.ClientRepository;
 
 import javax.persistence.NoResultException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService  {
@@ -39,5 +42,31 @@ public class ClientService  {
         }
 
         throw new SomethingWentWrongException("Client with this email already exists!");
+    }
+
+    public Response makeAdmin(Long clientId) {
+        Client client = getById(clientId);
+        client.setRole("ROLE_ADMIN");
+        clientRepository.update(client);
+        return new Response("Client " + client.getEmail() + " is now admin!");
+    }
+
+    public Response disable(Long clientId) {
+        Client client = getById(clientId);
+        client.setActivated(false);
+        clientRepository.update(client);
+        return new Response("Client deleted!");
+    }
+
+    private Client getById(Long clientId) {
+        return clientRepository.getById(clientId);
+    }
+
+    public AllClientsDTO getAllClients() {
+        List<ClientDTO> clients = clientRepository.getAllByActivated(true)
+                .stream()
+                .map(client -> new ClientDTO(client.getId(), client.getEmail(), client.getRole()))
+                .collect(Collectors.toList());
+        return new AllClientsDTO(clients);
     }
 }
