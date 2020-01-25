1a82a04d4563e51d66b08c0f3b448c5df5a7af7c
Elvinas Lukasevicius <ell@open.ch>
2020-01-25 16:10:00 +0200

    Weights filter cleanup

--- src/main/java/recommendator/services/algorithm/filters/WeightsFilter.java
+++ src/main/java/recommendator/services/algorithm/filters/WeightsFilter.java
@@ -1,23 +1,10 @@
 package recommendator.services.algorithm.filters;
 
-import recommendator.models.containers.RowWithPoints;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import recommendator.services.algorithm.FiltersData;
-import recommendator.services.BehaviorService;
-
-import java.util.Collections;
-import java.util.Comparator;
 
 @Service
 public class WeightsFilter implements AlgorithmFilter {
-
-    private BehaviorService behaviorService;
-
-    @Autowired
-    public WeightsFilter(BehaviorService behaviorService) {
-        this.behaviorService = behaviorService;
-    }
 
     @Override
     public FiltersData filter(FiltersData filtersData) {
@@ -29,10 +16,6 @@ public class WeightsFilter implements AlgorithmFilter {
             });
             row.setPoints(row.getPoints() * row.getWeight());
         });
-
-        // Sort list by points
-        Comparator<RowWithPoints> compareByPoints = Comparator.comparing(RowWithPoints::getPoints).reversed();
-        Collections.sort(filtersData.getRows(), compareByPoints);
 
         return filtersData;
     }
