5b5a608fcb668a2eede7973e14e6e8f1cb7dcfd0
Elvinas Lukasevicius <ell@open.ch>
2020-01-25 16:14:55 +0200

    Weights filter cleanup

--- src/main/java/recommendator/services/algorithm/filters/WeightsFilter.java
+++ src/main/java/recommendator/services/algorithm/filters/WeightsFilter.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 import recommendator.services.algorithm.FiltersData;
 
 @Service
+/**
+ *  Sums upp cell, columnName (column) and row weights. After that it also takes row points into account.
+**/
 public class WeightsFilter implements AlgorithmFilter {
 
     @Override
@@ -11,9 +14,12 @@ public class WeightsFilter implements AlgorithmFilter {
 
         filtersData.getRows().forEach(row -> {
             row.getCells().forEach(cell -> {
-                float weight = cell.getColumnName().getWeight() * cell.getWeight() / 20f;
-                row.setPoints(row.getPoints() * weight / 100);
+                // multiplies column weight with cell weight (totalColWeight)
+                float totalColWeight = cell.getColumnName().getWeight() * cell.getWeight() / 20f;
+                // multiplies row points with totalColWeight
+                row.setPoints(row.getPoints() * totalColWeight / 100);
             });
+            // multiplies row points with row weight
             row.setPoints(row.getPoints() * row.getWeight());
         });
 
